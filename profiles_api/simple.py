b04b8f878001d7e11eec19aec1fe3e32414bab66
Akumar <anilkumarejamandla2@gmail.com>
2024-10-02 11:29:05 +0530

    sample 3 function

--- profiles_api/simple.py
+++ profiles_api/simple.py
@@ -5,3 +5,5 @@ def sample_fun():
     print('Sample function from sample.py')
 def sample_fun2():
     print('Sample function2 from sample.py')
+def sample_fun3():
+    print('Sample function3 from sample.py')
